11156aeff0a163435d012ec8c304cc2af09aba78
Nam Son Le <z5684369@ad.unsw.edu.au>
2026-02-23 20:18:51 +1100

    Made prelimary tests

--- backend/autotester/src/test.spec.ts
+++ backend/autotester/src/test.spec.ts
@@ -22,6 +22,38 @@ describe("Task 1", () => {
       const response = await getTask1("");
       expect(response.status).toBe(400);
     });
+
+    // My personal extra tests that I have made
+
+    it("meatball", async () => {
+      const response = await getTask1("meatball");
+      expect(response.body).toStrictEqual({ msg: "Meatball" });
+    });
+
+    it("Skibidi spaghetti", async () => {
+      const response = await getTask1("Skibidi spaghetti");
+      expect(response.body).toStrictEqual({ msg: "Skibidi Spaghetti" });
+    });
+
+    it("$$$meat #ball$", async () => {
+      const response = await getTask1("$$$meat #ball$");
+      expect(response.body).toStrictEqual({ msg: "Meat Ball" });
+    });
+
+    it("Skibidi --spaghetti", async () => {
+      const response = await getTask1("Skibidi --spaghetti");
+      expect(response.body).toStrictEqual({ msg: "Skibidi Spaghetti" });
+    });
+
+    it("Skibidi_____spaghetti", async () => {
+      const response = await getTask1("Skibidi_____spaghetti");
+      expect(response.body).toStrictEqual({ msg: "Skibidi Spaghetti" });
+    });
+
+    it("@@@####  ###$$$", async () => {
+      const response = await getTask1("@@@####  ###$$$");
+      expect(response.status).toBe(400);
+    });
   });
 });
 
@@ -53,7 +85,17 @@ describe("Task 2", () => {
       expect(resp1.status).toBe(200);
     });
 
-    it("Congratulations u burnt the pan pt2", async () => {
+    it("bad Recipe", async () => {
+      const meatball = {
+        type: "recipe",
+        name: "Meatball",
+        requiredItems: [{ name: "Beef", quantity: 1 }, { name: "Beef", quantity: 1 }],
+      };
+      const resp1 = await putTask2(meatball);
+      expect(resp1.status).toBe(400);
+    });
+
+    it("Congratulations u burnt the pan pt2 (cooktime)", async () => {
       const resp = await putTask2({
         type: "ingredient",
         name: "beef",
@@ -62,7 +104,7 @@ describe("Task 2", () => {
       expect(resp.status).toBe(400);
     });
 
-    it("Congratulations u burnt the pan pt3", async () => {
+    it("Congratulations u burnt the pan pt3 (type)", async () => {
       const resp = await putTask2({
         type: "pan",
         name: "pan",
@@ -71,7 +113,7 @@ describe("Task 2", () => {
       expect(resp.status).toBe(400);
     });
 
-    it("Unique names", async () => {
+    it("Unique names error", async () => {
       const resp = await putTask2({
         type: "ingredient",
         name: "Beef",
@@ -125,6 +167,19 @@ describe("Task 3", () => {
       expect(resp2.status).toBe(400);
     });
 
+    it("exists reciple but wrong one", async () => {
+      const cheese = {
+        type: "recipe",
+        name: "Cheese",
+        requiredItems: [{ name: "Not Real", quantity: 1 }],
+      };
+      const resp1 = await postEntry(cheese);
+      expect(resp1.status).toBe(200);
+
+      const resp2 = await getTask3("Banana");
+      expect(resp2.status).toBe(400);
+    });
+
     it("Unknown missing item", async () => {
       const cheese = {
         type: "recipe",
@@ -156,6 +211,127 @@ describe("Task 3", () => {
 
       const resp3 = await getTask3("Skibidi");
       expect(resp3.status).toBe(200);
+      expect(resp3.body).toStrictEqual({
+        "name": "Skibidi",
+        "cookTime": 2,
+        "ingredients": [
+          {
+            "name": "Bruh",
+            "quantity": 1
+          }
+        ]
+      });
     });
+
+    it("Multiplication", async () => {
+      const item1 = {
+        type: "recipe",
+        name: "One",
+        requiredItems: [{ name: "Two", quantity: 2 }, { name: "Four", quantity: 3 }],
+      };
+      const resp1 = await postEntry(item1);
+      expect(resp1.status).toBe(200);
+
+      const item2 = {
+        type: "recipe",
+        name: "Two",
+        requiredItems: [{ name: "Three", quantity: 3 }, { name: "Four", quantity: 2 }],
+      };
+      const resp2 = await postEntry(item2);
+      expect(resp2.status).toBe(200);
+
+      const resp4 = await postEntry({
+        type: "ingredient",
+        name: "Three",
+        cookTime: 2,
+      });
+      expect(resp4.status).toBe(200);
+
+      const resp5 = await postEntry({
+        type: "ingredient",
+        name: "Four",
+        cookTime: 1,
+      });
+      expect(resp5.status).toBe(200);
+
+      const respFin = await getTask3("One");
+      expect(respFin.status).toBe(200);
+      expect(respFin.body).toStrictEqual({
+        "name": "One",
+        "cookTime": 19,
+        "ingredients": [
+          {
+            "name": "Four",
+            "quantity": 7
+          }, 
+          {
+            "name": "Three", 
+            "quantity": 6
+          }
+        ]
+      });
+      
+    });
+
+    it("Recursive", async () => {
+      const item1 = {
+        type: "recipe",
+        name: "One",
+        requiredItems: [{ name: "Two", quantity: 1 }],
+      };
+      const resp1 = await postEntry(item1);
+      expect(resp1.status).toBe(200);
+
+      const item2 = {
+        type: "recipe",
+        name: "Two",
+        requiredItems: [{ name: "Three", quantity: 1 }],
+      };
+      const resp2 = await postEntry(item2);
+      expect(resp2.status).toBe(200);
+
+      const item3 = {
+        type: "recipe",
+        name: "Three",
+        requiredItems: [{ name: "Four", quantity: 1 }, { name: "Five", quantity: 2 }],
+      };
+      const resp3 = await postEntry(item1);
+      expect(resp3.status).toBe(200);
+
+      const resp4 = await postEntry({
+        type: "ingredient",
+        name: "Four",
+        cookTime: 2,
+      });
+      expect(resp4.status).toBe(200);
+
+      const resp5 = await postEntry({
+        type: "ingredient",
+        name: "Five",
+        cookTime: 1,
+      });
+      expect(resp5.status).toBe(200);
+
+      const respFin = await getTask3("One");
+      expect(respFin.status).toBe(200);
+      expect(respFin.body).toStrictEqual({
+        "name": "One",
+        "cookTime": 4,
+        "ingredients": [
+          {
+            "name": "Four",
+            "quantity": 1
+          }, 
+          {
+            "name": "Five", 
+            "quantity": 2
+          }
+        ]
+      });
+
+      
+    });
+
+    
   });
 });
